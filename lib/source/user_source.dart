cdfc7c467d943f587699a025dec550e00b5e7da1
JanBu <yosefsitumorang97@gmail.com>
2023-01-12 22:46:54 +0700

    source regisert user

---
+++ lib/source/user_source.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:course_discuss_app/config/api.dart';
+import 'package:d_method/d_method.dart';
+import 'package:http/http.dart';
+
+class UserSource {
+  static Future<Map> register(String username, String password) async {
+    String url = '${Api.user}/register.php';
+    try {
+      Response response = await Client().post(Uri.parse(url), body: {
+        'username': username,
+        'password': password,
+      });
+      DMethod.printTitle('User Source - Register', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (err) {
+      DMethod.printTitle('User Source - Register', err.toString());
+      return {'success': false};
+    }
+  }
+}
