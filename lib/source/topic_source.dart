391c10678c0271a7a264289b0cd82ef939182656
JanBu <yosefsitumorang97@gmail.com>
2023-01-14 08:55:08 +0700

    source create topic

---
+++ lib/source/topic_source.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:d_method/d_method.dart';
+import 'package:http/http.dart';
+
+import '../config/api.dart';
+
+class TopicSource {
+  static Future<bool> create(
+    String title,
+    String description,
+    String images,
+    String base64codes,
+    String idUser,
+  ) async {
+    String url = '${Api.topic}/create.php';
+    try {
+      Response response = await Client().post(Uri.parse(url), body: {
+        'title': title,
+        'description': description,
+        'images': images,
+        'base64codes': base64codes,
+        'id_user': idUser,
+      });
+      DMethod.printTitle('Topic Source - create', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody['success'];
+    } catch (err) {
+      DMethod.printTitle('Topic Source - create', err.toString());
+      return false;
+    }
+  }
+}
