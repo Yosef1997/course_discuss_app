f60275351cc1407c71f381c310e210c5471503f3
JanBu <yosefsitumorang97@gmail.com>
2023-01-22 23:26:56 +0700

    fix app route register

--- lib/config/app_route.dart
+++ lib/config/app_route.dart
@@ -52,7 +52,7 @@ class AppRoute {
       ),
       GoRoute(
         path: register,
-        builder: (context, state) => const RegisterPage(),
+        builder: (context, state) => RegisterPage(),
       ),
       GoRoute(
         path: addTopic,
